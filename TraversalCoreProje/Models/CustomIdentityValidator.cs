af1130702255bfbcafb9bfc3b007705191c1b092
Ulas Can Nacaksiz <ulascannacaksiz@hotmail.com>
2022-10-26 16:16:30 +0300

    41.Dersle devam

---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography.Xml;
+
+namespace TraversalCoreProje.Models
+{
+    public class CustomIdentityValidator:IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Parola Minimum {length} karakter olmalıdır"
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parola en az bir küçük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Parola en az bir büyük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola en az bir özel karakter içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola en az bir sayı içermelidir."
+            };
+        }
+    }
+}
---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen adınızı giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adın giriniz")]
+        public string Username { get; set; 
+        }
+        [Required(ErrorMessage = "Lütfen mail adresini giriniz")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifre giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi terkrar giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler eşleşmiyor")]
+        public string ConfrimPassword { get; set; }
+
+    }
+}
